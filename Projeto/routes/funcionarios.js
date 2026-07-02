3009f33f618be4223c5ff31b9b2fe61305c83092
Miguel BaSilv <165849138+MigasBS@users.noreply.github.com>
2026-07-01 23:36:00 -0300

    Update funcionarios.js

--- Projeto/routes/funcionarios.js
+++ Projeto/routes/funcionarios.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/funcionariosControllers");
 
+router.post("/", controller.criarFuncionario);
+
 router.delete("/:id", controller.deletarFuncionario);
 
 router.put("/:id", controller.atualizarFuncionario);
