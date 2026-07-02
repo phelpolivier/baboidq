16d78d476d33ae35fc0585a71b3e4e3f03bb4789
MLLPEREIRA <miguellavoisierapps@gmail.com>
2026-07-02 00:10:55 -0300

    Update funcionarios.js
    Criação das rotas GET

--- Projeto/routes/funcionarios.js
+++ Projeto/routes/funcionarios.js
@@ -6,6 +6,10 @@ router.post("/", controller.criarFuncionario);
 
 router.delete("/:id", controller.deletarFuncionario);
 
+router.get("/:id", controller.buscarFuncionario);
+
+router.get("/:id", controller.listarFuncionarios);
+
 router.put("/:id", controller.atualizarFuncionario);
 
 module.exports = router;
